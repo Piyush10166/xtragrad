dff94b8999601dd4fffa7f5454a9216637c056f6
kamlesh <kgurjar1947@gmail.com>
2025-03-05 02:47:50 +0530

    sepration done

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index');
 });
 
